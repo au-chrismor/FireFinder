2f5f0e437b71195e940bcd5f8700c5a88383d5ee
Christopher Moran <chris.moran@emergent.com.au>
2022-01-09 22:06:04 +1100

    Added dir search and add target folders

--- audio_plotter/audio_plotter.py
+++ audio_plotter/audio_plotter.py
@@ -4,27 +4,35 @@ import matplotlib.pyplot as plt
 import scipy.io
 import scipy.io.wavfile
 import numpy as np
-import matplotlib.pyplot as ply
 
 audio_file = 'purenvwh.wav'
 # audio_file = '20210109_232215.wav'
-dataset_path = '/data/audio/Lightning-Detection/whistles'
+dataset_path = '/data/audio/lightning-Detection'
+output_path = '/data/audio/Lightning-Image-Format'
 
-wave_data = os.path.join(dataset_path, audio_file)
+for dr in os.listdir(dataset_path):
+    local_path = '{0}/{1}'.format(dataset_path, dr)
+    for file in os.listdir(local_path):
+        if file.endswith('.wav'):
+            wave_data = '{0}/{1}/{2}'.format(dataset_path, dr, file)
+            image_data = '{0}/{1}/{2}.png'.format(output_path, dr, file)
 
-sample_rate, audio_buffer = scipy.io.wavfile.read(wave_data)
-duration = len(audio_buffer) / sample_rate
+            print('Input File: {0}'.format(wave_data))
+            print('Output File: {0}'.format(image_data))
 
-print('Sample Rate: {0}'.format(sample_rate))
-print('Duration: {0}'.format(duration))
+            sample_rate, audio_buffer = scipy.io.wavfile.read(wave_data)
+            duration = len(audio_buffer) / sample_rate
 
-time = np.arange(0, duration, 1 / sample_rate)
+            print('Sample Rate: {0}'.format(sample_rate))
+            print('Duration: {0}'.format(duration))
 
-plt.plot(time, audio_buffer)
-plt.xlabel('Time')
-plt.ylabel('Amplitude')
-plt.axis("off")
-#plt.title(audio_file)
-plt.savefig('{0}.png'.format(audio_file))
-plt.show()
+            time = np.arange(0, duration, 1 / sample_rate)
 
+            plt.plot(time, audio_buffer)
+            plt.xlabel('Time')
+            plt.ylabel('Amplitude')
+            plt.axis("off")
+            #plt.title(audio_file)
+            plt.savefig(image_data)
+            #plt.show()
+            plt.
